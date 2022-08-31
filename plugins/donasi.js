edb252e5348a89a6b0055f9b5a228694be8a0437
Kahfi-XD <107167852+Kahfi-XD@users.noreply.github.com>
2022-08-31 15:20:35 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Tri [0895336282144]
-│ • Gopay  [0895336282144]
-│ • Dana  [0895336282144]
-│ • Saweria  [https://saweria.co/thesadboy01]
+│ • Telkom1 [085380166282]
+│ • Telkom2  [085295374964]
+│ • Dana  [085380166282]
+│ • Saweria  [https://saweria.co/kahfiaja]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895336282144
+│ > Ingin donasi? wa.me//6285380166282
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
